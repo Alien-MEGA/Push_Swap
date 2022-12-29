9ed9ad515bee63b340447cbff8a1a2b9a83c3b8b
Radouane Iben-hamou <redouanebh4@gmail.com>
2022-12-29 23:35:02 +0100

    tmp

--- ft_push_swap.c
+++ ft_push_swap.c
@@ -6,7 +6,7 @@
 /*   By: reben-ha <reben-ha@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 16:23:40 by reben-ha          #+#    #+#             */
-/*   Updated: 2022/12/29 23:09:43 by reben-ha         ###   ########.fr       */
+/*   Updated: 2022/12/29 23:34:08 by reben-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,10 +71,17 @@ int	main(int argc, char *argv[])
 	while (head_of_b)
 		p_ab(&head_of_b, &head_of_a, 'a');
 	// ft_print_lst(head_of_a, 'A');
-	// ft_print_lst(head_of_b, 'B');jbknk
+	// ft_print_lst(head_of_b, 'B');
 
 }
-	
+
+// find LIS
+// move not LIS to 
+
+
+
+
+
 	// //Test for linked list
 	// printf("\n\n\n\nStart :\n\n");
 	// head_of_b = NULL;
