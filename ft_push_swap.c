7390ac74c0088617d7315f4369c59e77462d380b
Radouane Iben-hamou <redouanebh4@gmail.com>
2023-01-13 17:19:25 +0100

    Push-Swap

--- ft_push_swap.c
+++ ft_push_swap.c
@@ -6,13 +6,13 @@
 /*   By: reben-ha <reben-ha@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 16:23:40 by reben-ha          #+#    #+#             */
-/*   Updated: 2023/01/13 16:20:51 by reben-ha         ###   ########.fr       */
+/*   Updated: 2023/01/13 17:18:30 by reben-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void ft_print_lst(t_list *head, int option)
+void ft_print_lst(t_list *head, int option) // delete this
 {	
 	while (head != NULL)
 	{
@@ -104,7 +104,6 @@ void	ft_instra(t_list **stack_a, t_list **stack_b)
 			else if (max_n->index > (len_a / 2))
 				target->instr_s += (len_a - (max_n->index)) - 1;
 			target->sub_index = (-1) * (max_n->index);
-			printf("\n\n\n\n+ %d + %d\n\n\n", max_n->index, max_n->data);
 		}
 		else
 		{
@@ -142,26 +141,18 @@ int	main(int argc, char *argv[]) // Add function to check
 	int		*expected;
 	int		i;
 	int		j;
+	t_list	*target;
+	t_list	*position;
+	int		len_a;
+	int		len_b;
 
-	if (argc <= 1) // if there just 1 arg
-		return (write(1, "Error\n", 6), 0);
-// make arg to linked list
+	if (argc <= 1)
+		return (write(2, "Error\n", 6), 0);
 	i = 1;
 	while (i < argc)
 		ft_lstadd_back(&stack_a, (ft_lstnew(ft_atoi(argv[i++]))));
-/*	
-	* 1 : Find LIS
-	* 2 : Move not LIS to stack B
-	* 3 : Find fast way number that can moving to here 
-			place with lowest possible number of operations
-*/
-// add some info
 	tmp_a = stack_a;
 	ft_indexing(tmp_a);
-//test
-	ft_print_lst(stack_a, 'A');
-//                                  * 1 : Find LIS
-// find LIS
 	i = 1;
 	while (in(stack_a, i))
 	{
@@ -184,9 +175,6 @@ int	main(int argc, char *argv[]) // Add function to check
 		in(stack_a, i)->sub_index = sub_index;
 		i++;
 	}
-//test
-	ft_print_lst(stack_a, 'A');
-// find len of LIS and the node that has max len_lis
 	max_lis = -1;
 	i = -1;
 	j = -1;
@@ -197,9 +185,6 @@ int	main(int argc, char *argv[]) // Add function to check
 		if (max_lis == in(stack_a, i)->lis)
 			break;
 	}
-//test
-	ft_print_lst(stack_a, 'A');
-// move LIS to array
 	expected = (int *)malloc(max_lis * sizeof(int));
 	if (!expected)
 		return (write(1, "Error\n", 6) ,0);
@@ -211,10 +196,6 @@ int	main(int argc, char *argv[]) // Add function to check
 		i = in(stack_a, i)->sub_index;
 		j--;
 	}
-//test
-	ft_print_lst(stack_a, 'A');
-//                                  * 2 : Move not LIS to stack B
-
 	i = 1;
 	while (stack_a->h_lis != 1)
 	{
@@ -226,34 +207,17 @@ int	main(int argc, char *argv[]) // Add function to check
 			rr_ab(&stack_a, &stack_b, 'a');
 		}
 		else
-			p_ab(&stack_a, &stack_b, 'a');
+			p_ab(&stack_a, &stack_b, 'b');
 	}
-//indexing
 	tmp_a = stack_a;
 	ft_indexing(tmp_a);
 	tmp_b = stack_b;
 	ft_indexing(tmp_b);
-//test
-	ft_print_lst(stack_a, 'A');
-	ft_print_lst(stack_b, 'B');
-
-// 						* 3 : Find fast way number that can moving to here 
-// 								place with lowest possible number of operations
-	t_list	*target;
-	t_list	*position;
-	int		len_a;
-	int		len_b;
-
-
 	while (stack_b)
 	{
 		ft_indexing(stack_a);
 		ft_indexing(stack_b);
 		ft_instra(&stack_a, &stack_b);
-		//test
-		ft_print_lst(stack_a, 'A');
-		ft_print_lst(stack_b, 'B');
-		// 
 		target = stack_b;
 		len_a = ft_lstsize(stack_a);
 		len_b = ft_lstsize(stack_b);
@@ -261,13 +225,6 @@ int	main(int argc, char *argv[]) // Add function to check
 		while (in(stack_b, ++j))
 			target = target->instr_s > in(stack_b, j)->instr_s ? in(stack_b, j) : target;
 		position = in(stack_a, (target->sub_index));
-		
-
-// seg fault ?? 
-// and handle case (if the target is big than max number in stack a)
-
-
-		printf("\ntarget = %p\v position = %p\v stack_a = %p\v stack_b = %p\v\nStart :{ \n", target, position, stack_a, stack_b);
 		if ((target->index <= (len_b / 2)) && (position->index <= (len_a / 2)))
 			while (!(stack_b->data == target->data) && !(stack_a->data == position->data))
 				rr_ab(&stack_a, &stack_b, 'X');
@@ -286,62 +243,23 @@ int	main(int argc, char *argv[]) // Add function to check
 		else if ((position->index > (len_a / 2)))
 			while (!(stack_a->data == position->data))
 				rrr_ab(&stack_a, &stack_b, 'a');
-
 		p_ab(&stack_b, &stack_a, 'a');
-		
 		if (target->sub_index < 0)
 			ss_ab(stack_a, stack_b, 'a');
-			
-		
-
-		printf("\n} End\n");
 	}
-
 	target = stack_a;
 	i = -1;
 	while (in(stack_a, ++i))
-	{
 		if (target->data > in(stack_a, i)->data)
 			target = in(stack_a, i);
-	}
 	target->h_lis = 1;
-	printf("\n = %d\n", target->data);
-
-	
 	len_a = ft_lstsize(stack_a);
-
 	if (target->index <= (len_a / 2))
 		while (!(stack_a->h_lis == 1))
 			rr_ab(&stack_a, &stack_b, 'a');
 	else if (target->index > (len_a / 2))
 		while (!(stack_a->h_lis == 1))
 			rrr_ab(&stack_a, &stack_b, 'a');
-
-
-
-
-
-
-				// printf("\n%d === %d", stack_a->data, position->data);
-
-	// ft_instra(&stack_a, &stack_b);
-
-// //test
-	ft_print_lst(stack_a, 'A');
-	ft_print_lst(stack_b, 'B');
-
-
-
-
-
-
-
-
-
-
-
-
-
-	// ft_print_lst(head_of_a, 'A');
-	// ft_print_lst(head_of_b, 'B');
+	// ft_print_lst(stack_a, 'A');
+	// ft_print_lst(stack_b, 'B');
 }
